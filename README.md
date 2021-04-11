eae2941e460e4dcf090543962d8353e25d99688d
Squirrel <laszlolazuer@Squirrels-MacBook-Pro.local>
2021-04-11 13:00:20 -0500

    chore(read): stopping point

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Review course
 
 ## Reference Material
 Here’s the link: https://github.com/15Dkatz/es6-tutorial/
+
+`LUD`: 4/11/21
+### Stopping point: Completed `section 3`
--- app/index.js
+++ app/index.js
@@ -1,2 +1,10 @@
-console.log('hello webpack');
-console.log('hello in the browser');
+var a = 'hello';
+var b = 'world';
+var c = a + ' ' + b;
+console.log(c)
+
+let d = `hello ${b}`
+console.log(d)
+
+let e = `${a} world`;
+console.log(e);
